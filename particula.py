e3aaef0bd9fe1e29ceee907c6cab5744905ad6b0
Adriel <adrielrodrigues156@gmail.com>
2026-05-01 11:46:33 -0300

    Implementação da classe Particle com métodos para atualizar velocidade e posição

--- particula.py
+++ particula.py
@@ -1,1 +1,22 @@
+import random
 # Define a classe Particle (posição, velocidade, pBest)
+# Rascunho: w : Fator de inéricia, v = velocidade atual da partícula;
+# c1 = fator cognitivo, puxa a partícula para o seu melhor resultado pessoal;
+# c2 = fator social, puxa a partícula para o melhor resultado do grupo;
+# r1 e r2 = números aleatórios entre 0 e 1;
+
+class particle:
+
+    def __init__(self,posicao,velocidade,pBest):
+        self.posicao = posicao
+        self.velocidade = velocidade
+        self.pBest = pBest
+    
+    def nova_velocidade(self, w, c1,c2, gBest):
+        r1 = random.random()
+        r2 = random.random()
+
+        self.velocidade = (w * self.velocidade + c1 * r1 * (self.pBest - self.posicao) + c2 * r2 * (gBest - self.posicao))
+
+    def nova_posicao(self):
+        self.posicao = self.posicao + self.velocidade
